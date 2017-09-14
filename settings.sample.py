4e629c18868ecbd45d523837f81cc26874d66460
csiesheep <csiegoat@gmail.com>
2017-09-14 09:25:50 +0000

    (main) add settings.sample.py

---
+++ settings.sample.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- encoding: utf8 -*-
+
+
+__author__ = 'sheep'
+
+
+DB_NAME = 'yelp'
+
+BUSINESS_COLL = 'business'
+USER_COLL = 'user'
+REVIEW_COLL = 'review'
+TIP_COLL = 'tip'
+CHECKIN_COLL = 'checkin'
+VECTOR_COLL = 'vector'
+
+BUSINESS_FILE = 'res/small_dataset/yelp_academic_dataset_business.json'
+USER_FILE = 'res/small_dataset/yelp_academic_dataset_user.json'
+REVIEW_FILE = 'res/small_dataset/yelp_academic_dataset_review.json'
+TIP_FILE = 'res/small_dataset/yelp_academic_dataset_tip.json'
+CHECKIN_FILE = 'res/small_dataset/yelp_academic_dataset_checkin.json'
+
+SEQ2ID_FILE = 'res/small_dataset/yelp_id_names.txt'
+SEQ2VEC_FILE = 'res/small_dataset/yelp_id_vectors.txt'
