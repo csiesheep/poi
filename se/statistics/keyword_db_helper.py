9c8fbd7b26e891133a28b835ec555725085ac5a7
Daniel Colom <djc5627@psu.edu>
2017-10-09 21:54:41 -0400

    Added get_top_keywords function to keyword_db_helper.py

--- se/statistics/keyword_db_helper.py
+++ se/statistics/keyword_db_helper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- encoding: utf8 -*-
-'''from db.db_helper import mongodb_helper
+
+from db.db_helper import mongodb_helper
 import settings
 import math
 import re, string
@@ -10,4 +11,21 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from se.statistics import distribution
 
-'''
+
+
+###################################################################
+# Added By: Dan Colom
+# Function Name: get_top_keywords
+# Description: get the top k keywords for a business
+#              based on TFIDF scores
+# Parameter: dist - list of keywords distributions
+#			 k - integer number of top keywords to return
+# Return: top_k - sorted list of top k keywords and their frequency
+###################################################################
+def get_top_keywords(dist, k):
+    top_k = []
+    for _ in k:
+        top_k.append(0)
+    for iter in range(0, k-1):
+        top_k[iter][0] = dist[iter]
+    return top_k
